1855b224185cbc0cc0bb883a6e9f9d2b735d938f
Ravi Dikshit <dixitravi@gmail.com>
2026-04-30 16:00:10 +0530

    Update events.js

--- data/events.js
+++ data/events.js
@@ -17,19 +17,19 @@ window.EVENTS = [
 { personId: "person::anvika", name: "Anvika Behl", type: "birthday", family: "Behl Family", familyHead: "Mandeep Behl", relation: "Daughter", date: "2000-08-23" },
 
 /* =======================
-   SAGGU FAMILY – Narender (Head: Narender Saggu)
+   SAGGU FAMILY (Head: Narendra Saggu)
 ======================== */
 {
-  personId: "couple::narender-palvinder",
-  name: "Narender Saggu & Palvinder Saggu",
+  personId: "couple::narendra-palvinder",
+  name: "Narendra Saggu & Palvinder Kaur",
   type: "anniversary",
   family: "Saggu Family",
-  familyHead: "Narender Saggu",
+  familyHead: "Narendra Saggu",
   date: "2001-01-28"
 },
-{ personId: "person::narender", name: "Narender Saggu", type: "birthday", family: "Saggu Family", familyHead: "Narender Saggu", relation: "Husband", date: "1971-09-23" },
-{ personId: "person::palvinder", name: "Palvinder Saggu", type: "birthday", family: "Saggu Family", familyHead: "Narender Saggu", relation: "Wife", date: "2000-08-08" },
-{ personId: "person::tejas", name: "Tejas Saggu", type: "birthday", family: "Saggu Family", familyHead: "Narender Saggu", relation: "Son", date: "2000-10-13" },
+{ personId: "person::narendra", name: "Narendra Saggu", type: "birthday", family: "Saggu Family", familyHead: "Narendra Saggu", relation: "Husband", date: "1971-09-23" },
+{ personId: "person::palvinder", name: "Palvinder Kaur", type: "birthday", family: "Saggu Family", familyHead: "Narendra Saggu", relation: "Wife", date: "2000-08-08" },
+{ personId: "person::tejas", name: "Tejas Saggu", type: "birthday", family: "Saggu Family", familyHead: "Narendra Saggu", relation: "Son", date: "2000-10-13" },
 
 /* =======================
    DIKSHIT FAMILY (Head: Ravi Dikshit)
@@ -52,52 +52,57 @@ window.EVENTS = [
 ======================== */
 {
   personId: "couple::bobby-paramjit",
-  name: "Bobby Singh & Paramjit Singh",
+  name: "Bobby Singh & Paramjit Kaur",
   type: "anniversary",
   family: "Bobby Singh Family",
   familyHead: "Bobby Singh",
   date: "2002-02-23"
 },
 { personId: "person::bobby", name: "Bobby Singh", type: "birthday", family: "Bobby Singh Family", familyHead: "Bobby Singh", relation: "Husband", date: "1974-07-09" },
-{ personId: "person::paramjit", name: "Paramjit Singh", type: "birthday", family: "Bobby Singh Family", familyHead: "Bobby Singh", relation: "Wife", date: "1980-08-24" },
-{ personId: "person::guneet", name: "Guneet Singh", type: "birthday", family: "Bobby Singh Family", familyHead: "Bobby Singh", relation: "Daughter", date: "2003-07-24" },
-{ personId: "person::sargun", name: "Sargun Singh", type: "birthday", family: "Bobby Singh Family", familyHead: "Bobby Singh", relation: "Daughter", date: "2008-09-12" },
+{ personId: "person::paramjit", name: "Paramjit Kaur", type: "birthday", family: "Bobby Singh Family", familyHead: "Bobby Singh", relation: "Wife", date: "1980-08-24" },
+{ personId: "person::guneet", name: "Guneet Kaur", type: "birthday", family: "Bobby Singh Family", familyHead: "Bobby Singh", relation: "Daughter", date: "2003-07-24" },
+{ personId: "person::sargun", name: "Sargun Kaur", type: "birthday", family: "Bobby Singh Family", familyHead: "Bobby Singh", relation: "Daughter", date: "2008-09-12" },
 
 /* =======================
-   SAGGU FAMILY – Hardeep (Head: Hardeep Saggu)
+   SAGGU FAMILY (Head: Hardeep Saggu)
 ======================== */
 {
   personId: "couple::hardeep-talvinder",
-  name: "Hardeep Saggu & Talvinder Saggu",
+  name: "Hardeep Saggu & Talvinder Kaur",
   type: "anniversary",
   family: "Saggu Family",
   familyHead: "Hardeep Saggu",
   date: "1990-10-11"
 },
-{ personId: "person::hardeep", name: "Hardeep Saggu",   nickname: "Bade bhaiya", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Husband", date: "1961-04-05" },
-{ personId: "person::talvinder", name: "Talvinder Saggu", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Wife", date: "1968-08-19" },
+{ personId: "person::hardeep", name: "Hardeep Saggu", nickname: "Bade bhaiya", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Husband", date: "1961-04-05" },
+{ personId: "person::talvinder", name: "Talvinder Kaur", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Wife", date: "1968-08-19" },
 { personId: "person::bhupinderdeep", name: "Bhupinderdeep Saggu", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Son", date: "2000-01-06" },
 { personId: "person::gagandeep", name: "Gagandeep Saggu", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Son", date: "2000-10-17" },
-{ personId: "person::prabhleen", name: "Prabhleen Saggu", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Daughter-in-law", date: "2000-05-09" },
+{ personId: "person::prabhleen", name: "Prabhleen Kaur", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Daughter-in-law", date: "2000-05-09" },
 { personId: "person::gauri", name: "Gauri Saggu", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Daughter-in-law", date: "2000-11-18" },
-{ personId: "person::jeeva", name: "Jeeva Saggu", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Grandchild", date: "2000-10-03" },
+{ personId: "person::jeeva", name: "Jeeva Kaur", type: "birthday", family: "Saggu Family", familyHead: "Hardeep Saggu", relation: "Grandchild", date: "2000-10-03" },
 
-{
-  personId: "couple::bhupinderdeep-prabhleen",
-  name: "Bhupinderdeep Saggu & Prabhleen Saggu",
-  type: "anniversary",
-  family: "Saggu Family",
-  familyHead: "Hardeep Saggu",
-  date: "2000-01-29"
-},
-{
-  personId: "couple::gagandeep-gauri",
-  name: "Gagandeep Saggu & Gauri Saggu",
-  type: "anniversary",
-  family: "Saggu Family",
-  familyHead: "Hardeep Saggu",
-  date: "2000-12-06"
-},
+	/* =======================
+	   SAGGU FAMILY – Hardeep (Anniversaries)
+	======================== */
+
+	{
+	  personId: "couple::bhupinderdeep-prabhleen",
+	  name: "Bhupinderdeep Saggu & Prabhleen Kaur",
+	  type: "anniversary",
+	  family: "Saggu Family",
+	  familyHead: "Hardeep Saggu",
+	  date: "2000-01-29"
+	},
+	{
+	  personId: "couple::gagandeep-gauri",
+	  name: "Gagandeep Saggu & Gauri Saggu",
+	  type: "anniversary",
+	  family: "Saggu Family",
+	  familyHead: "Hardeep Saggu",
+	  date: "2000-12-06"
+	},
+	
 
 /* =======================
    CHADHA FAMILY (Head: Ashish Chadha)
@@ -110,42 +115,10 @@ window.EVENTS = [
   familyHead: "Ashish Chadha",
   date: "2004-02-07"
 },
-{
-  personId: "person::ashish-chadha",
-  name: "Ashish Chadha",
-  type: "birthday",
-  family: "Chadha Family",
-  familyHead: "Ashish Chadha",
-  relation: "Husband",
-  date: "1978-10-06"
-},
-{
-  personId: "person::dina-chadha",
-  name: "Dina Chadha",
-  type: "birthday",
-  family: "Chadha Family",
-  familyHead: "Ashish Chadha",
-  relation: "Wife",
-  date: "1978-09-29"
-},
-{
-  personId: "person::naisha-chadha",
-  name: "Naisha Chadha",
-  type: "birthday",
-  family: "Chadha Family",
-  familyHead: "Ashish Chadha",
-  relation: "Daughter",
-  date: "2000-04-08"
-},
-{
-  personId: "person::navya-chadha",
-  name: "Navya Chadha",
-  type: "birthday",
-  family: "Chadha Family",
-  familyHead: "Ashish Chadha",
-  relation: "Daughter",
-  date: "2000-08-29"
-},
+{ personId: "person::ashish-chadha", name: "Ashish Chadha", type: "birthday", family: "Chadha Family", familyHead: "Ashish Chadha", relation: "Husband", date: "1978-10-06" },
+{ personId: "person::dina-chadha", name: "Dina Chadha", type: "birthday", family: "Chadha Family", familyHead: "Ashish Chadha", relation: "Wife", date: "1978-09-29" },
+{ personId: "person::naisha-chadha", name: "Naisha Chadha", type: "birthday", family: "Chadha Family", familyHead: "Ashish Chadha", relation: "Daughter", date: "2000-04-08" },
+{ personId: "person::navya-chadha", name: "Navya Chadha", type: "birthday", family: "Chadha Family", familyHead: "Ashish Chadha", relation: "Daughter", date: "2000-08-29" },
 
 /* =======================
    KOHLI FAMILY (Head: Navneet Kohli)
@@ -158,42 +131,10 @@ window.EVENTS = [
   familyHead: "Navneet Kohli",
   date: "2004-12-05"
 },
-{
-  personId: "person::navneet-kohli",
-  name: "Navneet Kohli",
-  type: "birthday",
-  family: "Kohli Family",
-  familyHead: "Navneet Kohli",
-  relation: "Husband",
-  date: "1977-07-25"
-},
-{
-  personId: "person::nalini-kohli",
-  name: "Nalini Kohli",
-  type: "birthday",
-  family: "Kohli Family",
-  familyHead: "Navneet Kohli",
-  relation: "Wife",
-  date: "1981-10-03"
-},
-{
-  personId: "person::anahita-kohli",
-  name: "Anahita Kohli",
-  type: "birthday",
-  family: "Kohli Family",
-  familyHead: "Navneet Kohli",
-  relation: "Daughter",
-  date: "2006-05-26"
-},
-{
-  personId: "person::avishka-kohli",
-  name: "Avishka Kohli",
-  type: "birthday",
-  family: "Kohli Family",
-  familyHead: "Navneet Kohli",
-  relation: "Daughter",
-  date: "2012-04-02"
-},
+{ personId: "person::navneet-kohli", name: "Navneet Kohli", type: "birthday", family: "Kohli Family", familyHead: "Navneet Kohli", relation: "Husband", date: "1977-07-25" },
+{ personId: "person::nalini-kohli", name: "Nalini Kohli", type: "birthday", family: "Kohli Family", familyHead: "Navneet Kohli", relation: "Wife", date: "1981-10-03" },
+{ personId: "person::anahita-kohli", name: "Anahita Kohli", type: "birthday", family: "Kohli Family", familyHead: "Navneet Kohli", relation: "Daughter", date: "2006-05-26" },
+{ personId: "person::avishka-kohli", name: "Avishka Kohli", type: "birthday", family: "Kohli Family", familyHead: "Navneet Kohli", relation: "Daughter", date: "2012-04-02" },
 
 /* =======================
    YETTI SINGH FAMILY (Head: Yetti Singh)
@@ -206,44 +147,14 @@ window.EVENTS = [
   familyHead: "Yetti Singh",
   date: "2003-05-12"
 },
-{
-  personId: "person::yetti-singh",
-  name: "Yetti Singh",
-  type: "birthday",
-  family: "Yetti Singh Family",
-  familyHead: "Yetti Singh",
-  relation: "Husband",
-  date: "1976-09-02"
-},
-{
-  personId: "person::meenal-singh",
-  name: "Meenal Singh",
-  type: "birthday",
-  family: "Yetti Singh Family",
-  familyHead: "Yetti Singh",
-  relation: "Wife",
-  date: "1976-11-01"
-},
-{
-  personId: "person::tanjil-singh",
-  name: "Tanjil Singh",
-  type: "birthday",
-  family: "Yetti Singh Family",
-  familyHead: "Yetti Singh",
-  relation: "Daughter",
-  date: "2000-06-29"
-},
-{
-  personId: "person::sara-singh",
-  name: "Sara Singh",
-  type: "birthday",
-  family: "Yetti Singh Family",
-  familyHead: "Yetti Singh",
-  relation: "Daughter",
-  date: "2000-04-11"
-},
+{ personId: "person::yetti-singh", name: "Yetti Singh", type: "birthday", family: "Yetti Singh Family", familyHead: "Yetti Singh", relation: "Husband", date: "1976-09-02" },
+{ personId: "person::meenal-singh", name: "Meenal Singh", type: "birthday", family: "Yetti Singh Family", familyHead: "Yetti Singh", relation: "Wife", date: "1976-11-01" },
+{ personId: "person::tanjil-singh", name: "Tanjil Singh", type: "birthday", family: "Yetti Singh Family", familyHead: "Yetti Singh", relation: "Daughter", date: "2000-06-29" },
+{ personId: "person::sara-singh", name: "Sara Singh", type: "birthday", family: "Yetti Singh Family", familyHead: "Yetti Singh", relation: "Daughter", date: "2000-04-11" }
 
 ];
+
+
 
 (function validateEvents(events) {
   const seenIds = new Set();
